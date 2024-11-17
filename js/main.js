8fcf5d686070cba91cefdfa4472b05d285fcdd7a
Dmytro Zinoviev <dzinoviev@ukr.net>
2024-11-17 22:50:00 +0200

    header done

---
+++ js/main.js
@@ -0,0 +1,59 @@
+'use strict';
+
+const logo = document.querySelector('.logo');
+let logoWidth = logo.offsetWidth
+
+const searchBtn = document.querySelector('.search');
+const searchInput = document.querySelector('.search-input');
+const searchImg = document.querySelector('.search > img.main-icon');
+const divSpanClose = document.querySelector('.close-icon');
+const searchIconHidden = document.querySelector('.search-icon-hidden');
+
+searchBtn.addEventListener('click', () => {
+    searchImg.style.display = 'none';
+    searchIconHidden.style.display = 'block';
+    searchInput.style.display = 'block';
+    searchInput.style.width = '100%';
+    if (window.innerWidth > 1024) {
+        searchBtn.style.width = `calc(100% - ${logoWidth + 50}px)`;
+    } else {
+        searchBtn.style.width = '100%';
+    }
+    searchBtn.style.background = '#0486e3';
+    searchBtn.style.position = 'absolute';
+    searchBtn.style.right = '0px';
+    divSpanClose.style.display = 'flex';
+});
+
+console.log(divSpanClose)
+divSpanClose.addEventListener('click', (event) => {
+    event.stopPropagation();
+    searchImg.style.display = 'block';
+    searchIconHidden.style.display = 'none';
+    searchInput.style.display = 'none';
+    searchInput.style.width = '0px';
+    searchBtn.style.width = '60px';
+    searchBtn.style.background = '#0095ff';
+    searchBtn.style.right = '0px';
+    searchBtn.style.right = '0px';
+    divSpanClose.style.display = 'none';
+    setTimeout(() => {
+        searchBtn.style.position = 'relative';
+    }, 50)
+});
+
+
+const mobileMenuBtn = document.querySelector('.mobile-menu-button');
+const overlay = document.querySelector('.overlay');
+const mainMenu = document.querySelector('.main-menu');
+
+mobileMenuBtn.addEventListener('click', () => {
+    if (overlay.classList.contains('visible')) {
+        overlay.classList.remove('visible');
+        mainMenu.classList.remove('visible');
+    } else {
+        overlay.classList.add('visible');
+        mainMenu.classList.add('visible');
+    }
+})
+
